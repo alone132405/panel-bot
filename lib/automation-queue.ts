43e72d5dae0691eab39dfc7cd2ad5d466c23f17a
hsaka si siht <ingakashyt1691@gmail.com>
2026-01-01 10:38:44 +0530

    dasas

--- lib/automation-queue.ts
+++ lib/automation-queue.ts
@@ -173,8 +173,8 @@ class AutomationQueue {
 
         const POPUP_FUNCTIONS_X = 159
         const POPUP_FUNCTIONS_Y = 60
-        const POPUP_RELOAD_X = 214
-        const POPUP_RELOAD_Y = 128
+        const POPUP_RELOAD_X = 176
+        const POPUP_RELOAD_Y = 153
 
         const scriptContent = `
 Add-Type -AssemblyName System.Windows.Forms
